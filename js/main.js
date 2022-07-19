e8f18e158b001152ee4de49dca8e72597b1312af
matheusgcoppi <matheuscoppi22@gmail.com>
2022-07-19 09:33:28 -0300

    update

--- js/main.js
+++ js/main.js
@@ -4,12 +4,20 @@ function createCalculator () {
 
         start() {
             alert('hello')
-        }
+        },
+
+        clickButtons() {
+            document.addEventListener('click', (e) => {
+                const element = e.target; //it will show up all stuff What I click, so useful
+                if(element ==)
+            });
+        },
     }
 }
 
 const calculator = createCalculator();
 calculator.start();
+calculator.clickButtons();
 
 
 
