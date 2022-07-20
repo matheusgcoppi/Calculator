6eb9fc36e1f2aa4bd04f046ef7fb81aa28be2da5
matheusgcoppi <matheuscoppi22@gmail.com>
2022-07-20 18:49:03 -0300

    Finished

--- js/main.js
+++ js/main.js
@@ -1,28 +1,64 @@
 function createCalculator () {
     return {
         display: document.querySelector('.display'),
+        clear: document.querySelector('.btn-clear'),
 
         start() {
-            alert('hello')
+            alert('hello');
+            this.clickButtons();
         },
 
         clickButtons() {
-            document.addEventListener('click', (e) => {
+            //this => calculator
+            document.addEventListener('click', function(e) {
                 const element = e.target; //it will show up all stuff What I click, so useful
+            //this => document
                 if(element.classList.contains('btn-num')) {
-                    this.buttonToDisplay();
+                    this.buttonToDisplay(element.innerText); //I need to use .This because when one method calls another method, e.g: if I want inicialize clickButtons() inside the start() I will use .this like in this case
                 }
-            }.bind(this));
-        },
-        buttonToDisplay(value) {
+                
+                if(element.classList.contains('btn-clear')) this.clearDisplay();
 
+                if(element.classList.contains('btn-del')) this.deleteOne();
+
+                if(element.classList.contains('btn-equal')) this.equalDisplay();
+
+            }.bind(this)) //I use .bind because in this case we can represent the display/calculator and without the .bind this will point to the document and not the calculator
+        },
+        
+        buttonToDisplay(value) {
+            this.display.value += value;
+        },
+
+        clearDisplay() {
+            this.display.value = ''
+        },
+
+        deleteOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        equalDisplay() {
+            //let count = this.display.value;
+
+            try {
+                this.display.value = eval(this.display.value);
+
+              if(!this.display.value) {
+                alert('Invalid count, please try again');
+                return;
+            }
+            } catch (error) {
+                alert('Invalid count, please try again');
+                return;
+            }
         }
     }
 }
 
 const calculator = createCalculator();
 calculator.start();
-calculator.clickButtons();
+
 
 
 
